02e4a256e5cdd950a3c7f538ca0b565aaf746d33
gunhee1113 <gunhee2001@snu.ac.kr>
2025-05-09 10:24:50 +0000

    add script for exp

---
+++ scripts/deform_mpm.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+BASE_PATH="/131_data/intern/gunhee/PhysTrack/New"
+CUDA_DEVICE=0  # Parameterize CUDA device number here
+
+SOLVER="MPM"
+ITEMS=("${SOLVER}/bouncing_balls" "${SOLVER}/cow" "${SOLVER}/falling_jelly" "${SOLVER}/pancake")
+
+# Mono - Traj
+OUTPUT_PATH="output/mono/traj"
+for ITEM in "${ITEMS[@]}"; do
+    CUDA_VISIBLE_DEVICES=${CUDA_DEVICE} python train.py -s "${BASE_PATH}/${ITEM}" -m "${OUTPUT_PATH}/${ITEM}" \
+        --init_with_traj --num_views single --eval --test_iterations 0
+    CUDA_VISIBLE_DEVICES=${CUDA_DEVICE} python render.py -m "${OUTPUT_PATH}/${ITEM}" --mode render
+    CUDA_VISIBLE_DEVICES=${CUDA_DEVICE} python metrics.py -m "${OUTPUT_PATH}/${ITEM}"
+done
+
+# Multi - Traj
+OUTPUT_PATH="output/multi/traj"
+for ITEM in "${ITEMS[@]}"; do
+    CUDA_VISIBLE_DEVICES=${CUDA_DEVICE} python train.py -s "${BASE_PATH}/${ITEM}" -m "${OUTPUT_PATH}/${ITEM}" \
+        --init_with_traj --num_views double --eval --test_iterations 0
+    CUDA_VISIBLE_DEVICES=${CUDA_DEVICE} python render.py -m "${OUTPUT_PATH}/${ITEM}" --mode render
+    CUDA_VISIBLE_DEVICES=${CUDA_DEVICE} python metrics.py -m "${OUTPUT_PATH}/${ITEM}"
+done
+
+# Mono - Colmap
+OUTPUT_PATH="output/mono/colmap"
+for ITEM in "${ITEMS[@]}"; do
+    CUDA_VISIBLE_DEVICES=${CUDA_DEVICE} python train.py -s "${BASE_PATH}/${ITEM}" -m "${OUTPUT_PATH}/${ITEM}" \
+        --num_views single --eval --test_iterations 0 
+    CUDA_VISIBLE_DEVICES=${CUDA_DEVICE} python render.py -m "${OUTPUT_PATH}/${ITEM}" --mode render
+    CUDA_VISIBLE_DEVICES=${CUDA_DEVICE} python metrics.py -m "${OUTPUT_PATH}/${ITEM}"
+done
+
+# Multi - Colmap
+OUTPUT_PATH="output/multi/colmap"
+for ITEM in "${ITEMS[@]}"; do
+    CUDA_VISIBLE_DEVICES=${CUDA_DEVICE} python train.py -s "${BASE_PATH}/${ITEM}" -m "${OUTPUT_PATH}/${ITEM}" \
+        --num_views double --eval --test_iterations 0
+    CUDA_VISIBLE_DEVICES=${CUDA_DEVICE} python render.py -m "${OUTPUT_PATH}/${ITEM}" --mode render
+    CUDA_VISIBLE_DEVICES=${CUDA_DEVICE} python metrics.py -m "${OUTPUT_PATH}/${ITEM}"
+done
